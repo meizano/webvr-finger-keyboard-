39d85922526a1bfec3dc8ccebbd0656d42b67e47
Meizano A.M. Djausal <meizano@users.noreply.github.com>
2026-08-14 16:54:32 +0800

    Create cardboard-v1.html backup and cardboard-v2.html featuring alphabet segment buttons attached directly on top of MediaPipe wireframe joints

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         cardboard: resolve(__dirname, 'cardboard.html'),
+        cardboardV1: resolve(__dirname, 'cardboard-v1.html'),
+        cardboardV2: resolve(__dirname, 'cardboard-v2.html'),
         webxr: resolve(__dirname, 'webxr.html'),
         mediapipeTest: resolve(__dirname, 'mediapipe-test.html')
       }
